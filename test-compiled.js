19516347a0e30b82eb4dcbdbfc7db62cfc4871e8
thisisouvik <souvikmandals10@gmail.com>
2026-08-30 22:19:56 +0530

    refactor: remove obsolete test files for compiled and executable contracts

--- test-compiled.js
+++
@@ -1,6 +0,0 @@
-const { Contract } = require('./contracts/artifacts/contract/index.js');
-const CompiledContract = require('@midnight-ntwrk/compact-js/effect/CompiledContract');
-
-let compiled = CompiledContract.make('verihealth', Contract);
-compiled = CompiledContract.withVacantWitnesses(compiled);
-console.log(compiled);
--- test-exec.js
+++
@@ -1,8 +0,0 @@
-const { Contract } = require('./contracts/artifacts/contract/index.js');
-const { ContractExecutable } = require('@midnight-ntwrk/compact-js/dist/cjs/effect/ContractExecutable.js');
-const contract = new Contract({});
-try {
-  console.log(ContractExecutable.make(contract));
-} catch(e) {
-  console.error(e.message);
-}
